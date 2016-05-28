ab072ace05ffa3c5a03c56982f2b51462edaa9d3
dandes3 <hopeisforsissies@icloud.com>
2016-05-28 18:16:02 -0400

    Working. All functions work correctly. Begin UI

--- pepper.py
+++ pepper.py
@@ -2,15 +2,25 @@
 # Created by Don Andes
 #
 
-# Define needed base values
+# This program accepts an ICS calender file and scrapes events from it. It then re-writes these events
+# into a nicely re-formatted new ICS file that includes additional information such as address
+# for work, appropriate name, and URL to login to time site. 
+# This software is provided FOR EDUCATIONAL USE ONLY, the writer assumes no risk or fault if you are late to your job.
+# That is your own fault. 
+
+
+# Define needed base values (They're chunky and they know it)
 baseString = ("BEGIN:VCALENDAR\nCALSCALE:GREGORIAN\nVERSION\nX-WR-CALNAME:Schedule\nBEGIN:VTIMEZONE\nTZID:America/New_York\nBEGIN:DAYLIGHT\nTZOFFSETFROM:-0500\nRRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=2SU\nDTSTART:20070311T020000\nTZNAME:EDT\nTZOFFSETTO:-0400\nEND:DAYLIGHT\nBEGIN:STANDARD\nTZOFFSETFROM:-0400\nRRULE:FREQ=YEARLY;BYMONTH=11;BYDAY=1SU\nDTSTART:20071104T020000\nTZNAME:EST\nTZOFFSETTO:-0500\nEND:STANDARD\nEND:VTIMEZONE\n")
-workLoc = ("300 Monticello Ave\, Macarthur Center\, Norfolk\, VA 23510")
+string2 = ("BEGIN:VEVENT\nUID:")
+string3 = ("\nDTEND;TZID=America/New_York:")
+string4 = ("\nTRANSP:OPAQUE\nX-APPLE-TRAVEL-ADVISORY-BEHAVIOR:AUTOMATIC\nSUMMARY:Work\nDTSTART;TZID=America/New_York:")
+string5 = ("\nLOCATION:300 Monticello Ave\, Macarthur Center\, Norfolk\, VA 23510\nDESCRIPTION:Work Schedule\nURL;VALUE=URI:https://mypage.apple.com\nEND:VEVENT\n")
+string6 = ("END:VCALENDAR\n")
 uidList = []
 eventList = []
 
 uidCount = 0;
 eventCount = 0;
-
 
 print("\n")
 print("Welcome to pepper.\n")
@@ -39,28 +49,27 @@ for line in oldIter:
 			eventList.append(filler)
 			eventCount+=1
 
+# Correction for occasional extra first UID
 if uidCount > eventCount:
 	uidList.pop(0)
 	uidCount-=1
 
-print("\n")
-print("UIDs scraped are: ")
-print(uidList)
-print("Number of UIDs are: ")
-print(uidCount)
-print("Events scraped are: ")
-print(eventList)
-print("Number of events are: ")
-print(eventCount)
-
-
-		
-
-
+# Close file and re-open as append mode for safety
 newFile.write(baseString)
+newFile.close()
+newFile = open("modified.ics", "a")
 
+# Constuct strings to write to file out of pre-made strings and scraped values
+for i in range(len(uidList)):
+	temp1 = string2+uidList[i]
+	temp2 = string3+eventList[i][1]
+	temp3 = string4+eventList[i][0]+string5
+	newFile.write(temp1+temp2+temp3)
+
+# Ends ics file
+newFile.write(string6)
 
 # Clean up work
 oldFile.close()
-newFile.close()
+
 
