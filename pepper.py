e6188a8a8500aea8151776bcc38b94c578dacb93
dandes3 <hopeisforsissies@icloud.com>
2016-07-11 15:57:53 -0400

    I broke everything

--- pepper.py
+++ pepper.py
@@ -23,14 +23,41 @@ import tkSimpleDialog
 # Pushes all Python windows to front in OS X 
 os.system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "Python" to true' ''')
 
-# Anchor for GUI window using TKinter. 
-master = Tk()
-master.wm_title("Pepper")
+
 
 def nuke():
-	''' Assures when the program is exited prematurely, no files are written. '''
-	sys.exit()
-	master.destroy()
+		''' Assures when the program is exited prematurely, no files are written. '''
+		sys.exit()
+		master.destroy()
+
+class Main():
+
+	def __init__(self, master):
+        self.master = master
+        self.master.title("Pepper")
+
+        self.button1 = Button(self.master, text="Click to select file", command = self.callback)
+        self.button1.grid(row=0, column=2, sticky=W)
+        #self.button2 = Button(self.master, text="Click Me 2", command = self.Open2)
+        # self.button2.grid(row=0, column=2, sticky=W)
+        # self.button3 = Button(self.master, text="Close", command = self.Close)
+        # self.button3.grid(row=1, column=0, sticky=W)
+
+	 
+	def Open1(self):
+	    second_window = Toplevel(self.master)
+	    window2 = Second(second_window)
+
+	def callback():
+		''' Creates button that throws a file selecton box to the user. Selected file will be old file to parse through.'''
+		global fileMod # Quick and dirty fix for encapsulaton of tk button functions
+		fileMod = tkFileDialog.askopenfilename() # Line modified for python 2 compatibility
+		master.quit()
+
+
+# Anchor for GUI window using TKinter. 
+master = Tk()
+#master.wm_title("Pepper")
 
 # Assigns closing function to behaviour of master window
 master.protocol("WM_DELETE_WINDOW", nuke)
@@ -54,14 +81,30 @@ try:
     configFileTester = open('config.txt')
     configFileTester.close()
 except IOError as e:
-	eventName = tkSimpleDialog.askstring("Initial Configuration", "Enter the name you want for the events")
-	if eventName == '':
-		nuke()
-	if eventName == ' ':
-		nuke()
-	configFile = open("config.txt", "w")
-	configFile.write(eventName)
-	configFile.close()
+	Label(master, text="First Name").grid(row=0)
+	Label(master, text="Last Name").grid(row=1)
+
+	e1 = Entry(master)
+	e2 = Entry(master)
+	e1.insert(10,"Miller")
+	e2.insert(10,"Jill")
+
+	e1.grid(row=0, column=1)
+	e2.grid(row=1, column=1)
+
+	Button(master, text='Cancel', command=nuke()).grid(row=3, column=0, sticky=W, pady=4)
+	Button(master, text='Show', command=show_entry_fields).grid(row=3, column=1, sticky=W, pady=4)
+
+	mainloop( )
+	# eventName = tkSimpleDialog.askstring("Initial Configuration", "Enter the name you want for the events")
+	# testBox = tkSimpleDialog.askstring("Test", "Test")
+	# if eventName == '':
+	# 	nuke()
+	# if eventName == ' ':
+	# 	nuke()
+	# configFile = open("config.txt", "w")
+	# configFile.write(eventName)
+	# configFile.close()
 
 # Pull name from config file
 with open('config.txt', 'r') as configFile:
@@ -70,12 +113,6 @@ with open('config.txt', 'r') as configFile:
 # Build new append string with event name from user
 string4 = string4a+eventName.rstrip("\n")+string4c
 
-
-def callback():
-	''' Creates button that throws a file selecton box to the user. Selected file will be old file to parse through.'''
-	global fileMod # Quick and dirty fix for encapsulaton of tk button functions
-	fileMod = tkFileDialog.askopenfilename() # Line modified for python 2 compatibility
-	master.quit()
 
 # Create and pack in dialog and button. Enter loop awaiting user file selection
 T = Text(master, height=7, width=50)
